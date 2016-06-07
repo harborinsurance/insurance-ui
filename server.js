0a2768252742f965d50b071a07db1c9a67f63921
Jeff Sloyer <jsloyer@gmail.com>
2016-06-06 21:37:45 -0400

    added api for getting rental applications

--- server.js
+++ server.js
@@ -1,10 +1,96 @@
-var express = require('express');
-var app = express();
+var express = require("express"),
+    app = express(),
+    dotenv = require("dotenv"),
+    cfenv = require("cfenv"),
+    Cloudant = require("cloudant"),
+    _ = require("underscore");
 
-app.use(express.static(__dirname + '/static'));
+dotenv.load();
 
-app.get('/api/currentTime', function(req, res) {
-  res.send({ time: new Date() });
+var appEnv = cfenv.getAppEnv();
+
+var cloudantCreds = appEnv.getServiceCreds("cloudant"),
+    dbName = "applications";
+
+app.use(express.static(__dirname + "/static"));
+
+app.get("/api/currentTime", function (request, response) {
+  response.send({ time: new Date() });
 });
 
-app.listen(process.env.PORT || 8080);
+app.get("/api/applications", function (request, response) {
+    db.view("applications", "all", function(error, body) {
+        if (!error) {
+            response.json(body.rows);
+        }
+        else {
+            response.error(error);
+        }
+    });
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("server started on port " + port);
+  var dbCreated = false;
+  Cloudant({account:cloudantCreds.username, password:cloudantCreds.password}, function(er, dbInstance) {
+      cloudant = dbInstance;
+      if (er) {
+          return console.log('Error connecting to Cloudant account %s: %s', cloudantCreds.username, er.message);
+      }
+
+      console.log('Connected to cloudant');
+      cloudant.ping(function(er, reply) {
+          if (er) {
+              return console.log('Failed to ping Cloudant. Did the network just go down?');
+          }
+
+          console.log('Server version = %s', reply.version);
+          console.log('I am %s and my roles are %j', reply.userCtx.name, reply.userCtx.roles);
+
+          cloudant.db.list(function(er, all_dbs) {
+              if (er) {
+                  return console.log('Error listing databases: %s', er.message);
+              }
+
+              _.each(all_dbs, function(name) {
+                  if (name === dbName) {
+                      dbCreated = true;
+                  }
+              });
+              if (dbCreated === false) {
+                  cloudant.db.create(dbName, seedDB);
+              }
+              else {
+                  db = cloudant.db.use(dbName);
+                  console.log("DB", dbName, "is already created");
+              }
+          });
+      });
+  });
+});
+
+function seedDB(callback) {
+  db = cloudant.use(dbName);
+
+  async.waterfall([
+    function (next) {
+      var designDocs = [
+          {
+            _id: '_design/applications',
+            views: {
+              all: {
+                map: function (doc) { emit(doc._id, doc) }
+              }
+            }
+          }
+     ];
+
+      async.each(designDocs, db.insert, next);
+    },
+    function (next) {
+      console.log("Created DB", dbName, "and populated it with initial purchases");
+      next();
+    }
+  ], callback)
+}
