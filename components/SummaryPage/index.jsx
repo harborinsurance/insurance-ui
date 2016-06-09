5aea75f90d28b4e8a42b19c9d56e0e5d46055183
Chris Dhanaraj <adhanaraj@gmail.com>
2016-06-09 13:46:58 -0500

    create new sub pages for the form

---
+++ components/SummaryPage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SummaryPage = (props) => {
+  return (
+    <div className="hello">
+      <p>SummaryPage</p>
+    </div>
+  )
+};
+
+export default SummaryPage;
