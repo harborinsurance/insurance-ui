c527c824f8dc7dcbe60e3c706622c61c16f5dbd2
Theodore Wilson <theodore.wilson@us.ibm.com>
2016-06-09 14:08:48 -0700

    stepper implemented for policy

--- components/Policy.jsx
+++ components/Policy.jsx
@@ -7,11 +7,13 @@ import axios from 'axios';
 
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
+import DatePicker from 'material-ui/DatePicker';
 import RaisedButton from 'material-ui/RaisedButton';
 import Divider from 'material-ui/Divider';
 import MenuItem from 'material-ui/MenuItem';
 import Paper from 'material-ui/Paper';
 import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import {Stepper, Step, StepLabel, StepContent} from 'material-ui/Stepper';
 
 import { makeFakeApplications } from '../src/helpers';
 
@@ -21,47 +23,8 @@ class Policy extends Component {
         super(props, context);
 
         this.state = {
+            stepIndex: 0
         };
-    }
-
-    render() {
-        return(
-            <Paper style={{padding:20, margin:20}} zDepth={2}>
-                <h2>Policy: {this.props.name}</h2>
-                <p>{this.props.description}</p>
-                <p><b>Cost:</b> ${this.props.cost}/month</p>
-                {this.props.paid ? null : <PolicyPaymentForm applicationID={this.props.applicationID} /> }
-            </Paper>
-        );
-    }
-}
-
-class PolicyPaymentForm extends Component {
-
-    constructor(props, context) {
-        super(props, context);
-        this.state = {
-            submitted: false
-        };
-    }
-
-    submitPayment() {
-
-        let payload = {
-            card: {
-                number: this.state.cardNumber,
-                exp_month: `0${this.state.expirationDate.getMonth()}`.slice(-2),
-                exp_year: this.state.expirationDate.getFullYear(),
-                cvc: this.secuirtyCode
-            }
-        };
-
-        axios.post(`/api/applications/${this.props.applicationID}/charge`, payload).then((res) => {
-            this.setState({submitted: true, error: null});
-        }).catch((e) => {
-            console.log(e);
-            this.setState({error: e});
-        });
     }
 
     handleChange (source, e, payload) {
@@ -74,31 +37,112 @@ class PolicyPaymentForm extends Component {
         this.setState(updateValues);
     }
 
-    render() {
-        let disableSubmit = (!(this.state.cardNumber && this.state.securityCode && this.state.expirationDate));
+    handleNext() {
+       const {stepIndex} = this.state;
+       this.setState({
+         stepIndex: stepIndex + 1,
+         finished: stepIndex >= 2
+       });
+     }
 
-        let content;
-        if (!this.state.submitted) {
-            content = (
-                <div>
-                    <TextField ref="cardNumber" floatingLabelText="Credit card" type="number" onChange={this.handleChange.bind(this, "cardNumber")}/>
-                    <br/>
-                    <TextField ref="securityCode" floatingLabelText="Security code" type="number" onChange={this.handleChange.bind(this, "securityCode")}/>
-                    <br/>
-                    <DatePicker ref="expirationDate" placeholder="Expiration date"  onChange={this.handleChange.bind(this, "expirationDate")}/>
-                    <br/>
-                    <RaisedButton ref="submit" label="Submit" onClick={this.submitPayment.bind(this)} disabled={disableSubmit} secondary={true}/>
-                </div>
-            );
-        } else {
-            content = (
-                <div>
-                    <h3>Thank you.  A representative will be in touch.</h3>
-                </div>
-            );
-        }
-        return (
-            <div>{content}</div>
+     handlePrev() {
+       const {stepIndex} = this.state;
+       if (stepIndex > 0) {
+         this.setState({stepIndex: stepIndex - 1});
+       }
+     }
+
+     renderStepActions(step) {
+       const {stepIndex} = this.state;
+
+       return (
+         <div style={{margin: '12px 0'}}>
+           <RaisedButton
+             label={stepIndex === 2 ? 'Finish' : 'Next'}
+             disableTouchRipple={true}
+             disableFocusRipple={true}
+             primary={true}
+             onTouchTap={this.handleNext}
+             style={{marginRight: 12}}
+           />
+           {step > 0 && (
+             <FlatButton
+               label="Back"
+               disabled={stepIndex === 0}
+               disableTouchRipple={true}
+               disableFocusRipple={true}
+               onTouchTap={this.handlePrev}
+             />
+           )}
+         </div>
+       );
+     }
+
+     submitPayment() {
+
+         let payload = {
+             card: {
+                 number: this.state.cardNumber,
+                 exp_month: `0${this.state.expMonth}`.slice(-2),
+                 exp_year: `20${this.state.expYear}`.slice(-4),
+                 cvc: this.secuirtyCode
+             }
+         };
+
+         axios.post(`/api/applications/${this.props.applicationID}/charge`, payload).then((res) => {
+             this.handleNext();
+         }).catch((e) => {
+             this.setState({error: e});
+         });
+     }
+
+
+    render() {
+        const {stepIndex} = this.state;
+        const disableSubmit = (!(this.state.cardNumber && this.state.securityCode && this.state.expMonth && this.state.expYear));
+        return(
+            <div>
+                <h2 style={{padding:20}}>Your application has been approved!</h2>
+                <Stepper activeStep={stepIndex} orientation="vertical">
+                    <Step>
+                        <StepLabel>Review Policy</StepLabel>
+                        <StepContent>
+                            <h3>{this.props.name}</h3>
+                            <p>{this.props.description}</p>
+                            <p><b>Cost:</b> ${this.props.cost}.00 / month</p>
+                            <RaisedButton className="next" label="Next" primary={true} onClick={this.handleNext.bind(this)} />
+                        </StepContent>
+                    </Step>
+                    <Step>
+                        <StepLabel>Submit Payment</StepLabel>
+                        <StepContent>
+                            <div>
+                                <TextField ref="cardNumber" floatingLabelText="Credit card" type="number" onChange={this.handleChange.bind(this, "cardNumber") } value={this.state.cardNumber}/>
+                                <br/>
+                                <TextField ref="securityCode" floatingLabelText="Security code" type="number" onChange={this.handleChange.bind(this, "securityCode") } value={this.state.securityCode}/>
+                                <br/>
+                                <TextField ref="expMonth" floatingLabelText="Expiration Month" type="number" onChange={this.handleChange.bind(this, "expMonth") } value={this.state.expMonth}/>
+                                <br />
+                                <TextField ref="expYear" floatingLabelText="Expiration Year" type="number" onChange={this.handleChange.bind(this, "expYear") } value={this.state.expYear}/>
+                                <br />
+                                <TextField floatingLabelText="Amount" value={`$ ${this.props.cost}.00 / month`} disabled={true}/>
+                                <br />
+                                <br />
+                                <div style={{display:"flex"}}>
+                                    <RaisedButton style={{marginRight:10}} className="prev" label="Back" secondary={true} onClick={this.handlePrev.bind(this)}/>
+                                    <RaisedButton style={{marginLeft:10}} ref="submit" label="Submit" onClick={this.submitPayment.bind(this)} disabled={disableSubmit} primary={true}/>
+                                </div>
+                            </div>
+                        </StepContent>
+                    </Step>
+                    <Step>
+                        <StepLabel>Complete</StepLabel>
+                        <StepContent>
+                            <h2>A representative will be in touch.</h2>
+                        </StepContent>
+                    </Step>
+                </Stepper>
+            </div>
         );
     }
 }
