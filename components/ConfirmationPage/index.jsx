cf2c6aea9f546f8ec9c8683fe03a6fc891c07cdc
Dallas Hudgens <dallashudgens@Dallass-MacBook-Pro.local>
2016-06-09 21:50:02 -0500

    Updated paper style as well as add a return to home button

--- components/ConfirmationPage/index.jsx
+++ components/ConfirmationPage/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router';
 import './style.scss';
 
 import Paper from 'material-ui/Paper';
@@ -11,6 +12,14 @@ const ConfirmationPage = (props) => {
       <h3>Thank you for your application!</h3>
       <p>We are reviewing your application and will send you a text message once your application has been reviewed.</p>
       </Paper>
+
+      <Link to="/home">
+        <RaisedButton
+          label="return to home"
+          secondary={true}
+          className="stepper-nav__btn"
+        />
+      </Link>
     </div>
   )
 };
