2f1a0e10a6bd806220dd2a0c45547dcfa4a64b0a
Chris Dhanaraj <adhanaraj@gmail.com>
2016-06-09 20:30:54 -0500

    fix currency

--- components/CoveragePage/index.jsx
+++ components/CoveragePage/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.scss';
 
 import DatePicker from 'material-ui/DatePicker';
@@ -8,21 +8,51 @@ import RaisedButton from 'material-ui/RaisedButton';
 import MenuItem from 'material-ui/MenuItem';
 import Paper from 'material-ui/Paper';
 
-const CoveragePage = (props) => {
-  return (
-  <div className="coverageForm">
-    <div>
-    <p>Enter your desired coverage amount</p>
-      <TextField className="text-field" floatingLabelText="Desired coverage amount" />
+class CoveragePage extends Component {
 
-      <div className="ssn">
-        <h3 className="important">Important Notice</h3>
-        <p>For coverage amounts greater than $100,000.00 (USD) a credit check is required.</p>
-        <TextField floatingLabelText="Social security number" />
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { fieldValues } = this.props;
+
+    let ssn = (
+      +fieldValues.coverage >= 100000 ? (
+        <div className="ssn">
+          <h3 className="important">Important Notice</h3>
+          <p>For coverage amounts greater than $100,000.00 (USD) a credit check is required.</p>
+
+          <TextField
+            name="socialSecurityNumber"
+            floatingLabelText="Social security number"
+            onChange={this.props.handleChange}
+            value={fieldValues.socialSecurityNumber}
+          />
+        </div>) : ( '' )
+
+    );
+
+    return (
+      <div className="coverageForm">
+        <div className="form-row">
+          <p>Enter your desired coverage amount</p>
+          <TextField
+            name="coverage"
+            className="text-field currency"
+            floatingLabelText="Desired coverage amount"
+            onChange={this.props.handleChange}
+            value={fieldValues.coverage}
+          />
+        </div>
+        <div className="form-row">
+          {ssn}
+        </div>
       </div>
-    </div>
-  </div>
-  )
+    )
+  }
+
+
 };
 
 export default CoveragePage;
