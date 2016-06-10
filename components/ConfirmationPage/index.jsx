71c135ab231be9b626c8a59058bce28a3dfac3fa
Chris Dhanaraj <adhanaraj@gmail.com>
2016-06-09 22:32:11 -0500

    Revert "Revert "Merging Development Branch with Master""

--- components/ConfirmationPage/index.jsx
+++ components/ConfirmationPage/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './style.scss';
+
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
 
 const ConfirmationPage = (props) => {
   return (
-    <div className="hello">
-      <p>ConfirmationPage</p>
+    <div>
+      <Paper className="confirm">
+      <h3>Thank you for your application!</h3>
+      <p>We are reviewing your application and will send you a text message once your application has been reviewed.</p>
+      </Paper>
     </div>
   )
 };
--- components/SummaryPage/index.jsx
+++ components/SummaryPage/index.jsx
@@ -1,11 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './style.scss';
 
-const SummaryPage = (props) => {
-  return (
-    <div className="hello">
-      <p>SummaryPage</p>
-    </div>
-  )
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import Paper from 'material-ui/Paper';
+
+class SummaryPage extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { fieldValues } = this.props;
+
+    // fieldValues.fistName,.. etc
+
+    const date = new Date(fieldValues.dateOfBirth);
+    const dateString = `${date.getMonth()}/${date.getDate()}/${date.getYear()}`;
+
+    return (
+      <div className="summary-form">
+          <div className="form-row">
+            <div className="form-row__item bold">Product </div>
+            <div className="form-row__item">{fieldValues.product} </div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-row__item bold">Coverage Amount</div>
+            <div className="form-row__item">{fieldValues.coverage} </div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-row__item bold">Zip Code </div>
+            <div className="form-row__item">{fieldValues.zipCode} </div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-row__item bold">Full Name </div>
+            <div className="form-row__item">{fieldValues.firstName} {fieldValues.lastName}</div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-row__item bold">Phone Number </div>
+            <div className="form-row__item">{fieldValues.phone} </div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-row__item bold">Email </div>
+            <div className="form-row__item">{fieldValues.email} </div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-row__item bold">Stress Address </div>
+            <div className="form-row__item">{fieldValues.streetAddress} {fieldValues.streetAddressCont}</div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-row__item bold">City </div>
+            <div className="form-row__item">{fieldValues.city} </div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-row__item bold">State </div>
+            <div className="form-row__item">{fieldValues.state} </div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-row__item bold">Date of Birth</div>
+            <div className="form-row__item">{dateString} </div>
+          </div>
+
+
+      </div>
+    )
+  }
+
 };
 
 export default SummaryPage;
