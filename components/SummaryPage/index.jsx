1fd355cabbbc5da0561d51cf86e62173b4fd0244
Dallas Hudgens <dallashudgens@Dallass-MacBook-Pro.local>
2016-06-09 20:41:46 -0500

    styling and content for summary page

--- components/SummaryPage/index.jsx
+++ components/SummaryPage/index.jsx
@@ -1,11 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './style.scss';
 
-const SummaryPage = (props) => {
-  return (
-    <div className="hello">
-      <p>SummaryPage</p>
-    </div>
-  )
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import Paper from 'material-ui/Paper';
+
+class SummaryPage extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { fieldValues } = this.props;
+
+    // fieldValues.fistName,.. etc
+
+    return (
+      <div className="SummaryForm">
+        <div className="form-row">
+          <div className="form-row__item bold">Product </div>
+          <div className="form-row__item">{fieldValues.product} </div>
+        </div>
+
+        <div className="form-row">
+          <div className="form-row__item bold">Zip Code </div>
+          <div className="form-row__item">{fieldValues.zipCode} </div>
+        </div>
+
+        <div className="form-row">
+          <div className="form-row__item bold">Full Name </div>
+          <div className="form-row__item">{fieldValues.firstName} {fieldValues.lastName}</div>
+        </div>
+
+        <div className="form-row">
+          <div className="form-row__item bold">Phone Number </div>
+          <div className="form-row__item">{fieldValues.phone} </div>
+        </div>
+
+        <div className="form-row">
+          <div className="form-row__item bold">Email </div>
+          <div className="form-row__item">{fieldValues.email} </div>
+        </div>
+
+        <div className="form-row">
+          <div className="form-row__item bold">Stress Address </div>
+          <div className="form-row__item">{fieldValues.streetAddress} {fieldValues.streetAddressCont}</div>
+        </div>
+
+        <div className="form-row">
+          <div className="form-row__item bold">City </div>
+          <div className="form-row__item">{fieldValues.city} </div>
+        </div>
+
+        <div className="form-row">
+          <div className="form-row__item bold">State </div>
+          <div className="form-row__item">{fieldValues.state} </div>
+        </div>
+
+        <div className="form-row form-row--padded">
+          <div className="form-row__item bold">Date of Birth</div>
+          <div className="form-row__item">{fieldValues.dateOfBirth} </div>
+        </div>
+      </div>
+    )
+  }
+
 };
 
 export default SummaryPage;
