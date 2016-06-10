d169f32de8704bea1592607c5cbc06c7d280198f
Dallas Hudgens <dallashudgens@Dallass-MacBook-Pro.local>
2016-06-09 21:29:59 -0500

    updated styles on summary page, removed from incorrect location in appPage styles

--- components/SummaryPage/index.jsx
+++ components/SummaryPage/index.jsx
@@ -13,54 +13,62 @@ class SummaryPage extends Component {
 
   render() {
     const { fieldValues } = this.props;
-    console.log(fieldValues);
+
+    // fieldValues.fistName,.. etc
 
     return (
-      <div className="SummaryForm">
-        <div className="form-row">
-          <div className="form-row__item bold">Product </div>
-          <div className="form-row__item">{fieldValues.product} </div>
-        </div>
+      <div className="summary-form">
+          <div className="form-row">
+            <div className="form-row__item bold">Product </div>
+            <div className="form-row__item">{fieldValues.product} </div>
+          </div>
 
-        <div className="form-row">
-          <div className="form-row__item bold">Zip Code </div>
-          <div className="form-row__item">{fieldValues.zipCode} </div>
-        </div>
+          <div className="form-row">
+            <div className="form-row__item bold">Coverage Amount</div>
+            <div className="form-row__item">{fieldValues.coverage} </div>
+          </div>
 
-        <div className="form-row">
-          <div className="form-row__item bold">Full Name </div>
-          <div className="form-row__item">{fieldValues.firstName} {fieldValues.lastName}</div>
-        </div>
+          <div className="form-row">
+            <div className="form-row__item bold">Zip Code </div>
+            <div className="form-row__item">{fieldValues.zipCode} </div>
+          </div>
 
-        <div className="form-row">
-          <div className="form-row__item bold">Phone Number </div>
-          <div className="form-row__item">{fieldValues.phone} </div>
-        </div>
+          <div className="form-row">
+            <div className="form-row__item bold">Full Name </div>
+            <div className="form-row__item">{fieldValues.firstName} {fieldValues.lastName}</div>
+          </div>
 
-        <div className="form-row">
-          <div className="form-row__item bold">Email </div>
-          <div className="form-row__item">{fieldValues.email} </div>
-        </div>
+          <div className="form-row">
+            <div className="form-row__item bold">Phone Number </div>
+            <div className="form-row__item">{fieldValues.phone} </div>
+          </div>
 
-        <div className="form-row">
-          <div className="form-row__item bold">Stress Address </div>
-          <div className="form-row__item">{fieldValues.streetAddress} {fieldValues.streetAddressCont}</div>
-        </div>
+          <div className="form-row">
+            <div className="form-row__item bold">Email </div>
+            <div className="form-row__item">{fieldValues.email} </div>
+          </div>
 
-        <div className="form-row">
-          <div className="form-row__item bold">City </div>
-          <div className="form-row__item">{fieldValues.city} </div>
-        </div>
+          <div className="form-row">
+            <div className="form-row__item bold">Stress Address </div>
+            <div className="form-row__item">{fieldValues.streetAddress} {fieldValues.streetAddressCont}</div>
+          </div>
 
-        <div className="form-row">
-          <div className="form-row__item bold">State </div>
-          <div className="form-row__item">{fieldValues.state} </div>
-        </div>
+          <div className="form-row">
+            <div className="form-row__item bold">City </div>
+            <div className="form-row__item">{fieldValues.city} </div>
+          </div>
 
-        <div className="form-row form-row--padded">
-          <div className="form-row__item bold">Date of Birth</div>
-          <div className="form-row__item">{fieldValues.dateOfBirth} </div>
-        </div>
+          <div className="form-row">
+            <div className="form-row__item bold">State </div>
+            <div className="form-row__item">{fieldValues.state} </div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-row__item bold">Date of Birth</div>
+            <div className="form-row__item">{fieldValues.dateOfBirth} </div>
+          </div>
+
+
       </div>
     )
   }
